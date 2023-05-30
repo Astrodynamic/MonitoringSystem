49f22e343ea53a25159cc38edcd641774af00a55
Astrodynamic <astrodynami.fkg@gmail.com>
2023-05-30 16:30:27 +0700

    change email send

--- model/manager/notification/notification_manager.cc
+++ model/manager/notification/notification_manager.cc
@@ -25,35 +25,30 @@ auto NotificationManager::sendTellegramNotification(const std::string msg)
   return success;
 }
 
-auto NotificationManager::sendEmailNotification(const std::string msg) -> bool {
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    // Установка параметров SMTP-сервера
-    curl_easy_setopt(curl, CURLOPT_URL, "smtp://smtp.example.com");
-    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, "sender@example.com");
-    struct curl_slist *recipients = NULL;
-    recipients = curl_slist_append(recipients, "recipient@example.com");
-    curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
-
-    // Установка тела письма
-    curl_easy_setopt(curl, CURLOPT_READFUNCTION, NULL);
-    curl_easy_setopt(curl, CURLOPT_READDATA, NULL);
-    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-    const char *payload_text = msg.c_str();
-    curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)strlen(payload_text));
-    // curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
-    curl_easy_setopt(curl, CURLOPT_READDATA, payload_text);
-
-    // Отправка письма
-    CURLcode res = curl_easy_perform(curl);
-
-    // Проверка результата отправки
-    if (res != CURLE_OK) {
-      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-    }
-
-    // Освобождение ресурсов
-    curl_slist_free_all(recipients);
-    curl_easy_cleanup(curl);
+auto NotificationManager::sendEmailNotification(const std::string& message) -> bool {
+  CURL* curl = curl_easy_init();
+  if (!curl) {
+    return false;
   }
+
+  curl_easy_setopt(curl, CURLOPT_URL, "smtp://smtp.example.com");
+  curl_easy_setopt(curl, CURLOPT_MAIL_FROM, "sender@example.com");
+  struct curl_slist* recipients = curl_slist_append(nullptr, "recipient@example.com");
+  curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+
+  curl_easy_setopt(curl, CURLOPT_READFUNCTION, nullptr);
+  curl_easy_setopt(curl, CURLOPT_READDATA, message.c_str());
+  curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+  curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, static_cast<curl_off_t>(message.length()));
+
+  CURLcode res = curl_easy_perform(curl);
+
+  if (res != CURLE_OK) {
+    fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+  }
+
+  curl_slist_free_all(recipients);
+  curl_easy_cleanup(curl);
+
+  return res == CURLE_OK;
 }
