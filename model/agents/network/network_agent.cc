e1e87016ba2022f06de596d2ac4222a2eade5dd4
SergeyZhirnov <sergey.zhirnov.2003@mail.ru>
2023-11-21 01:01:36 +0700

    Add network agent collect metric

--- model/agents/network/network_agent.cc
+++ model/agents/network/network_agent.cc
@@ -4,6 +4,23 @@
 extern "C" {
 #endif
 
+#include <stdio.h>
+#include <string.h>
+
+static int GetNetworkAvailabiliry() {
+  FILE* ping;
+  char buffer[128];
+  memset(&buffer, 0, 128);
+
+  ping = popen("ping 8.8.8.8 -c 1 | grep \"1 received\"", "r");
+  if (NULL != ping) {
+    fgets(buffer, 128, ping);
+    pclose(ping);
+  }
+
+  return (strlen(buffer) == 0) ? 0 : 1;
+}
+
 Agent* __create(const AgentSettings settings) {
   return new NetworkAgent(settings);
 }
@@ -15,5 +32,7 @@ Agent* __create(const AgentSettings settings) {
 NetworkAgent::NetworkAgent(const AgentSettings& settings) : Agent(settings) {}
 
 auto NetworkAgent::Metrics() -> const QHash<QString, Metric>& {
+  m_settings.m_metrics["network_availability"].value = GetNetworkAvailabiliry();
+
   return m_settings.m_metrics;
 }
